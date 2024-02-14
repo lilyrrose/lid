c83c8628f8a15153e5e2190f77025d7c1ef7f344
opusbopus <opusbopus@proton.me>
2024-02-14 10:35:41 -0500

    Add benchmarks of other libraries, better example, fix documentation, random config

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,17 +4,64 @@ use lid::LID;
 fn benchmark(c: &mut Criterion) {
     {
         let mut lid = LID::<12, 8>::new();
-        c.bench_function("lid.generate() (20 byte id)", |b| {
-            b.iter(|| black_box(lid.generate()));
-        });
+        c.bench_function(
+            "LID w/ 12 prefix, 8 sequence, min incr=100, max incr=1000",
+            |b| {
+                b.iter(|| black_box(lid.generate()));
+            },
+        );
     }
 
     {
         let mut lid = LID::<16, 12>::new();
-        c.bench_function("lid.generate() (28 byte id)", |b| {
-            b.iter(|| black_box(lid.generate()));
-        });
+        c.bench_function(
+            "LID w/ 16 prefix, 12 sequence, min incr=100, max incr=1000",
+            |b| {
+                b.iter(|| black_box(lid.generate()));
+            },
+        );
     }
+
+    {
+        let mut lid = LID::<12, 8, 50_000, 5_000_000>::new();
+        c.bench_function(
+            "LID w/ 12 prefix, 8 sequence, min incr=50_000, max incr=5_000_000",
+            |b| {
+                b.iter(|| black_box(lid.generate()));
+            },
+        );
+    }
+
+    {
+        let mut lid = LID::<16, 12, 50_000, 5_000_000>::new();
+        c.bench_function(
+            "LID w/ 16 prefix, 12 sequence, min incr=50_000, max incr=5_000_000",
+            |b| {
+                b.iter(|| black_box(lid.generate()));
+            },
+        );
+    }
+
+    c.bench_function("colorid w/ 20 bytes", |b| {
+        b.iter(|| black_box(colorid::colorid(20)));
+    });
+
+    c.bench_function("colorid w/ 28 bytes", |b| {
+        b.iter(|| black_box(colorid::colorid(28)));
+    });
+
+    c.bench_function("nanoid::nanoid!()", |b| {
+        b.iter(|| black_box(nanoid::nanoid!()));
+    });
+
+    c.bench_function("nanoid::nanoid!(28)", |b| {
+        b.iter(|| black_box(nanoid::nanoid!(28)));
+    });
+
+    c.bench_function("snowflaked (i64)", |b| {
+        let mut gen = snowflaked::Generator::new(6969);
+        b.iter(|| black_box(gen.generate::<i64>()));
+    });
 }
 
 criterion_group!(benches, benchmark);
--- examples/example.rs
+++ examples/example.rs
@@ -1,7 +1,18 @@
-use lid::LID;
+use lid::configs::{new_distributed, new_random};
 
 fn main() {
-    // Custom ID size.
-    let mut lid = LID::<12, 8>::new();
-    println!("{:?}", lid.generate());
+    // `new_distributed` gives a low randomness LID instance with the default size of 20 bytes.
+    // Don't use it if you don't want people potentially guessing next IDs.
+    let mut lid = new_distributed();
+    println!("{}", lid.generate());
+
+    // `new_random` gives a high randomness LID instance.
+    // Generic parameters are prefix and sequence length, combined is the total size of the id.
+    let mut lid = new_random::<6, 6>();
+    println!("{}", lid.generate());
+
+    // This uses static Mutex backed instances of the generators above.
+    // You must enable the `easy` feature to use these.
+    println!("{}", lid::easy::generate_distributed());
+    println!("{}", lid::easy::generate_random());
 }
